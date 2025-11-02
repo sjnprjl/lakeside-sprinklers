5dc1f9ca72d1a657827c35112d5f26581b074493
Sujan Parajuli <42593762+sjnprjl@users.noreply.github.com>
2025-11-02 02:26:08 -0500

    add: deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
 
+const basePath = process.env.NODE_ENV === "production" ? "/lakeside-sprinklers" : "";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://sjnprjl.github.io",
+  base: basePath,
   experimental: {
     fonts: [
       {
